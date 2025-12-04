44b1b8a3647ec00d9c2fd2023b2fa7c59417954a
Jim <nuttapon.srpn@gmail.com>
2025-12-04 21:17:17 +0700

    feat: add login page with username and password authentication

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -25,7 +25,7 @@ const handleLogin = () => {
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
       <div class="text-center mb-8">
-        <img src="https://scontent.fbkk35-1.fna.fbcdn.net/v/t39.30808-6/413810278_10211154767703037_834009150123622705_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=WxNz7qE-CQsQ7kNvwEK-Hzt&_nc_oc=AdmtImNdbQLoRz-vNVOhud1_xJ2I1C1dxT9o9so3wJWAqvKrBM0sjL1Hzsmr_gfahQFd-jJ_iBugrBQDxLNrFQYn&_nc_zt=23&_nc_ht=scontent.fbkk35-1.fna&_nc_gid=qP3-meSzGy8jdcMZeV6J_g&oh=00_AfjIy_1qURIDenUkjIl-mxxJrflSwHGuRIGbbQFTF0XtQg&oe=6930846C" alt="Kiki POS" class="h-32 w-32 mx-auto mb-4 rounded-full object-cover" />
+        <img src="https://scontent.fbkk28-1.fna.fbcdn.net/v/t39.30808-6/413810278_10211154767703037_834009150123622705_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=diV_k9AJLaQQ7kNvwERqb71&_nc_oc=Adn4KIe3b9VBDa4LONWlNS6q0AXbSTrZ9dGceB0fxkgCjSMbzgbCouSsAdBXtLJnKfI&_nc_zt=23&_nc_ht=scontent.fbkk28-1.fna&_nc_gid=lEfFXtp6pCZ-5D7lJi6Naw&oh=00_AfllK4nqT3bQfzY6ySHBIcsA4ADxS7URvvCuR7GMyp9jXA&oe=6937542C" alt="Kiki POS" class="h-32 w-32 mx-auto mb-4 rounded-full object-cover" />
         <p class="text-gray-500 mt-2">Sign in to Kiki Cheesecake - Market Place</p>
       </div>
 
